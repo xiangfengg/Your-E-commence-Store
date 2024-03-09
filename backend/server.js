03f76d853402e4915e142f0006812f67389de7bc
xiangfengg <yongxiangfeng13@gmail.com>
2024-03-10 03:02:02 +0800

    bugfix

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,17 @@ import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
-
+import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-const port = process.env.PORT 
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import cookieParser from 'cookie-parser';
+
+const port = process.env.PORT 
+
+connectDB();
 
 const app = express();
 
@@ -18,9 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //parse cookie send together with the request & make it available in the req.cookie object 
 app.use(cookieParser());
-// Enable CORS for all routes
 
-connectDB();
 
 // routes
 app.use('/api/products', productRoutes);
@@ -41,6 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
+  const __dirname = path.resolve();
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
